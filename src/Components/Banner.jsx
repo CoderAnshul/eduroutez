5dd296d44e5dc06dfd447cadd5db8f5df812f026
Anshul Sharma <anshul.sharma6163@gmail.com>
2024-11-30 14:08:06 +0530

    changes in home search bar

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import searchImg from "../assets/Images/searchLogo.png";
+import CustomButton from "../Ui components/CustomButton";
 
 const Banner = () => {
   return (
@@ -8,22 +9,25 @@ const Banner = () => {
         <h1 className="text-4xl text-white text-center font-semibold mb-5">
           Find Over 5000+ Colleges in India
         </h1>
-        <div className="search ml-5 mr-5 h-12 bg-white border-[1.5px] border-gray-500  rounded-lg items-center gap-2 max-w-[800px] w-full overflow-hidden flex">
-         <div className="flex items-center w-4/5 px-4 py-2 gap-3">
+        <div className="search ml-5 mr-5 h-12 bg-white border-[1.5px] relative border-gray-500  rounded-lg items-center gap-2 max-w-[800px] w-full overflow-hidden flex">
+         <div className="flex items-center w-4/5 pl-4 py-2 gap-3">
          <button className="hover:scale-105 transition-all">
             <img src={searchImg} alt="search"  />
           </button>
           <input
-            className="text-sm w-full"
+            className="text-sm w-1/2"
             type="text"
             name="search"
             id="search"
             placeholder="Search for Colleges , institute and more..."
           />
          </div>
-          <button className="h-full w-1/5 bg-red-500 min-w-24 hover:bg-red-400 hover:scale-105 transition-all text-white">
+          {/* <button className="h-full w-1/5 bg-red-500 min-w-24 hover:bg-red-400 hover:scale-105 transition-all text-white">
             Search
-          </button>
+          </button> */}
+          <CustomButton text='Search' to='/searchpage' className="!h-full right-0 !rounded-sm w-1/5 absolute top-0 bg-red-500 min-w-24 hover:bg-red-400 hover:scale-105 transition-all text-white">
+            Search
+          </CustomButton>
         </div>
       </div>
     </div>
