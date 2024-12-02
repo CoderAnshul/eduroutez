1faa4467fd1b9dfc8d8c16fc8facd056c4456fae
Anshul Sharma <anshul.sharma6163@gmail.com>
2024-12-02 15:45:04 +0530

    loader added for smooth transtion and react lazy addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,55 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import {Route, Routes, BrowserRouter } from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import Homepage from './Pages/Homepage';
 import Footer from './Components/Footer';
-import PageNotFound from './Pages/PageNotFound';
 import ScrollToTop from './Utilities/ScrollToTop';
-import Instituepage from './Pages/Instituepage';
-import SearchPage from './Pages/SearchPage';
-import Coursesinfopage from './Pages/Coursesinfopage';
-import Blogpage from './Pages/Blogpage';
-import Contactuspage from './Pages/Contactuspage';
-import Aboutus from './Pages/Aboutus';
-import Counselingpage from './Pages/Counselingpage';
-import QuestionandAnswer from './Pages/QuestionandAnswer';
-import Careerspage from './Pages/Careerspage';
-import Detailpage from './Pages/Detailpage';
-import Writereview from './Pages/Writereview';
-import Login from './Pages/Login';
-import Signup from './Pages/Signup';
-import Forgotpassword from './Pages/Forgotpassword';
+import Loader from './Components/Loader';
+
+const Homepage = React.lazy(() => import('./Pages/Homepage'));
+const PageNotFound = React.lazy(() => import('./Pages/PageNotFound'));
+const Instituepage = React.lazy(() => import('./Pages/Instituepage'));
+const SearchPage = React.lazy(() => import('./Pages/SearchPage'));
+const Coursesinfopage = React.lazy(() => import('./Pages/Coursesinfopage'));
+const Blogpage = React.lazy(() => import('./Pages/Blogpage'));
+const Contactuspage = React.lazy(() => import('./Pages/Contactuspage'));
+const Aboutus = React.lazy(() => import('./Pages/Aboutus'));
+const Counselingpage = React.lazy(() => import('./Pages/Counselingpage'));
+const QuestionandAnswer = React.lazy(() => import('./Pages/QuestionandAnswer'));
+const Careerspage = React.lazy(() => import('./Pages/Careerspage'));
+const Detailpage = React.lazy(() => import('./Pages/Detailpage'));
+const Writereview = React.lazy(() => import('./Pages/Writereview'));
+const Login = React.lazy(() => import('./Pages/Login'));
+const Signup = React.lazy(() => import('./Pages/Signup'));
+const Forgotpassword = React.lazy(() => import('./Pages/Forgotpassword'));
 
 const App = () => {
   return (
-    <BrowserRouter
-        future={{
-          v7_startTransition: true,
-          v7_relativeSplatPath: true
-        }}
-    >
-      <ScrollToTop/>
-      <Navbar/>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Navbar />
+      <Suspense fallback={<Loader />}>
         <Routes>
-          <Route path='/' element={<Homepage/>} />
-          <Route path='*' element={<PageNotFound/>} />
-          <Route path='/institute' element={<Instituepage/>} />
-          <Route path='/searchpage' element={<SearchPage/>} />
-          <Route path='/coursesinfopage' element={<Coursesinfopage/>} />
-          <Route path='/blogpage' element={<Blogpage/>} />
-          <Route path='/contactuspage' element={<Contactuspage/>} />
-          <Route path='/aboutus' element={<Aboutus/>} />
-          <Route path='/counselingpage' element={<Counselingpage/>} />
-          <Route path='/questionandAnswer' element={<QuestionandAnswer/>} />
-          <Route path='/careerspage' element={<Careerspage/>} />
-          <Route path='/detailpage' element={<Detailpage/>} />
-          <Route path='/writereview' element={<Writereview/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/signup' element={<Signup/>} />
-          <Route path='/forgotpassword' element={<Forgotpassword/>} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/institute" element={<Instituepage />} />
+          <Route path="/searchpage" element={<SearchPage />} />
+          <Route path="/coursesinfopage" element={<Coursesinfopage />} />
+          <Route path="/blogpage" element={<Blogpage />} />
+          <Route path="/contactuspage" element={<Contactuspage />} />
+          <Route path="/aboutus" element={<Aboutus />} />
+          <Route path="/counselingpage" element={<Counselingpage />} />
+          <Route path="/questionandAnswer" element={<QuestionandAnswer />} />
+          <Route path="/careerspage" element={<Careerspage />} />
+          <Route path="/detailpage" element={<Detailpage />} />
+          <Route path="/writereview" element={<Writereview />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgotpassword" element={<Forgotpassword />} />
         </Routes>
-        <Footer/>
+      </Suspense>
+      <Footer />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
