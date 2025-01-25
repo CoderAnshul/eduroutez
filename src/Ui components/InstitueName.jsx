e77ba3fab283493604aa63b4361b529aef10914d
Anshul Sharma <anshul.sharma6163@gmail.com>
2025-01-25 23:37:15 +0530

    institute overflow solved

--- src/Ui components/InstitueName.jsx
+++ src/Ui components/InstitueName.jsx
@@ -117,7 +117,7 @@ const InstitueName = ({ instituteData }) => {
 
       {/* Popup Form */}
       {isPopupVisible && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center backdrop-blur-sm z-[1000]">
           <div className="bg-white p-6 rounded-lg shadow-lg w-96">
             <h2 className="text-xl font-bold mb-4">Apply Now</h2>
             <form onSubmit={handleSubmit}>
