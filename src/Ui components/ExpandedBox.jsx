524f8adc2c76d498b89c1a9b9596c60844126ace
Anshul <you@example.com>
2025-01-27 18:41:42 +0530

    Refactor ExpandedBox component to simplify rendering and improve content display

--- src/Ui components/ExpandedBox.jsx
+++ src/Ui components/ExpandedBox.jsx
@@ -17,17 +17,10 @@ const ExpandedBox = ({ contentData ,instituteData}) => {
         className="overflow-hidden transition-all duration-300"
         style={{ height: isExpanded ? 'auto' : '100px' }}
       >
-        {/* {contentData.map((section, index) => (
-          <div key={index} className="mb-4">
-            <h3 className="text-lg font-bold">{section.title}</h3>
-            <p className="text-base">{section.content}</p>
-          </div>
-        ))} */}
         <div className="mb-4">
-            <h3 className="text-lg font-bold">{instituteData?.data?.instituteName || "College information"}</h3>
-            <p className="text-base">{instituteData?.data?.collegeInfo || "no data found"}</p>
-          </div>
-        {/* Bottom gradient */}
+          <h3 className="text-lg font-bold">{instituteData?.data?.instituteName || "College information"}</h3>
+          <p className="text-base" dangerouslySetInnerHTML={{ __html: instituteData?.data?.about || "no data found" }} />
+        </div>
         {!isExpanded && (
           <div className="absolute bottom-0 left-0 w-full h-12 bg-gradient-to-t from-white mb-10 to-transparent" />
         )}
