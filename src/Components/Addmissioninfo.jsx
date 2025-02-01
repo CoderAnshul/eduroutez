f617205a39e0975750238bee336cd2e863460af4
Anshul <you@example.com>
2025-02-01 15:59:50 +0530

    Update component padding for improved layout consistency

--- src/Components/Addmissioninfo.jsx
+++ src/Components/Addmissioninfo.jsx
@@ -10,7 +10,7 @@ const Addmissioninfo = ({instituteData}) => {
     //       </div>
     // </div>
     <div className="min-h-28 w-full flex flex-col justify-between bg-white shadow-[0px_0px_10px_rgba(0,0,0,0.1)] rounded-xl mb-2 p-4 pt-8">
-      <div className="mb-4">
+      <div className="mb-4  p-8">
         <h3 className="text-lg font-bold">Admission Information</h3>
         <div className="text-base prose prose-gray max-w-full">
           <div dangerouslySetInnerHTML={{ __html: instituteData?.data ? instituteData?.data?.admissionInfo : 'No data found' }} />
--- src/Components/ScholarshipInfo.jsx
+++ src/Components/ScholarshipInfo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ScholarshipInfo = ({instituteData}) => {
   return (
     <div className="min-h-28 w-full flex flex-col justify-between bg-white shadow-[0px_0px_10px_rgba(0,0,0,0.1)] rounded-xl mb-2 p-4 pt-8">
-      <div className="mb-4">
+      <div className="mb-4  p-8">
         <h3 className="text-lg font-bold">Scholarship opportunities</h3>
         <p className="text-base" dangerouslySetInnerHTML={{ __html: instituteData?.data ? instituteData?.data?.scholarshipInfo : 'no data found' }}></p>
       </div>
