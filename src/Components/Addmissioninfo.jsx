01868e594544417d47f19e5f8db76d173e4fe6e5
Anshul Sharma <anshul.sharma6163@gmail.com>
2025-02-16 22:28:01 +0530

    doc change done with navbar style update

--- src/Components/Addmissioninfo.jsx
+++ src/Components/Addmissioninfo.jsx
@@ -58,7 +58,7 @@ const renderHTML = (htmlContent) => {
 const Addmissioninfo = ({ instituteData }) => {
   return (
     <div className="min-h-28 w-full flex flex-col justify-between bg-white shadow-[0px_0px_10px_rgba(0,0,0,0.1)] rounded-xl mb-2 p-4 pt-8">
-      <div className="mb-4 p-8">
+      <div className="mb-4 md:p-8">
         <h3 className="text-lg font-bold">Admission Information</h3>
         {renderHTML(instituteData?.data ? instituteData?.data?.admissionInfo : 'No data found')}
       </div>
